cabb3821b82fa414ebc5b564b0ac81be47cc5ce5
xia <270197253@qq.com>
2021-09-02 12:11:29 +0800

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Component } from 'react';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state =  {
+      dishes:DISHES
+    };
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +22,7 @@ class App extends Component {
             <NavbarBrand href="/">OnePiece  Restaurant</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
